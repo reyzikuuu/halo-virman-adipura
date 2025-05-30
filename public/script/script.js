861d893f8ff5cf1727fba053b1afb818e9a9defb
El Reyzikuuu <virmanadipuraaa@gmail.com>
2025-05-30 21:58:13 +0700

    first commit

---
+++ public/script/script.js
@@ -0,0 +1,14 @@
+
+const textAnim = document.querySelector('.text-anim');
+
+// Animation Headline Text
+
+var typed = new Typed(textAnim,{
+  strings : [' Virman Adipura', 'Editor', 'Content Creator'],
+  typeSpeed : 130,
+  backSpeed : 50,
+  loop : true,
+});
+
+// Feather Icon
+feather.replace();
